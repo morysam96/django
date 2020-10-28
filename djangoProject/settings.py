ad29a2d618324b7abda2610e55d876706bf88dd1
mortaza samadpoor <morysam96@gmail.com>
2020-10-28 17:06:36 +0330

    add index.html to blog app

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # Static Files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 
 # CDEditor
